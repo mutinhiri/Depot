3fd6c962d681e1748c07c2c232135ffb11698077
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-27 23:44:01 +0200

    Add changes

--- db/migrate/20220227212538_create_products.rb
+++ db/migrate/20220227212538_create_products.rb
@@ -1,10 +1,11 @@
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
+      t.string :\
       t.string :title
-      t.text :description
+      t.text :escription
       t.string :image_url
-      t.decimal :price, precision: 8, scale: 2
+      t.decimal :price
 
       t.timestamps
     end
