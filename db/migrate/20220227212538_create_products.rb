875cf904f1dae7abbea9fb83b94ab7246df33732
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-27 23:26:45 +0200

    Add new migrations

---
+++ db/migrate/20220227212538_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :\
+      t.string :title
+      t.text :escription
+      t.string :image_url
+      t.decimal :price
+
+      t.timestamps
+    end
+  end
+end
