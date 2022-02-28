d685b4ee09e5d81407c220692a8a9ba882e7fa1f
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-28 11:38:06 +0200

    Add schena

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_27_212538) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_28_093159) do
+  create_table "product1s", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "image_url"
+    t.decimal "price", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
